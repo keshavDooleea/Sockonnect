c3f013d1d9a59da5efef9ae1e1ae49c55b7daa71
keshav <kdooleea@yahoo.ca>
2020-06-13 12:59:43 -0400

    designing login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,11 +5,9 @@ class Login extends Component {
     render() {
         return (
             <div className="auth_body">
-                <h1>Sockonnect</h1>
-                <div className="R D">
-                    <h1>R D</h1>
-                </div>
-                <div className="auth_div"></div>
+                <h1 className="login_h1">Login</h1>
+                <h1 className="reg_h1">Register</h1>
+                <div></div>
             </div>
         );
     }
