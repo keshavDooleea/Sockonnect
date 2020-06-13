cfb1cc42e10d828a0aacf86ff74b95c9b7b8c8ef
keshav <kdooleea@yahoo.ca>
2020-06-13 19:59:54 -0400

    eye toggled done

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
+import showPassword from "../assets/show_pass.png";
+import hidePassword from "../assets/hide_pass.png";
 import "../App.css"
 
 // https://dev.to/christiankastner/integrating-p5-js-with-react-i0d
 // 1280 x 610 
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isShowEye: false,
+        };
+
+        this.alternatePassword = this.alternatePassword.bind(this);
+    }
+
+    alternatePassword() {
+        // shadow
+        const span = document.querySelector("form div span");
+        const img = document.querySelector("form div span img");
+        const input = document.getElementsByName("password")[0];
+        span.style.boxShadow = "inset 6px 6px 13px #21a48e, inset -6px -6px 13px #2ddec0";
+
+        // toggle eye
+        if (!this.state.isShowEye) {
+            img.src = hidePassword;
+            input.type = "text";
+        } else {
+            img.src = showPassword;
+            input.type = "password";
+        }
+        this.setState({
+            isShowEye: !this.state.isShowEye
+        });
+
+        // remove shadow
+        setTimeout(() => {
+            span.style.boxShadow = "none";
+        }, 100);
+    }
+
     render() {
         return (
             <div className="auth_body">
@@ -27,7 +64,22 @@ class Login extends Component {
                             <h1>I</h1>
                             <h1>N</h1>
                         </div>
-                        <form></form>
+                        <form>
+                            <div>
+                                <label htmlFor="username">Username</label>
+                                <input type="text" name="username" className="login_username_input" spellCheck="false" autoComplete="off" />
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password</label>
+                                <input type="password" name="password" spellCheck="false" autoComplete="off" />
+                                <span>
+                                    <img src={showPassword} alt="showPass" onClick={this.alternatePassword} />
+                                </span>
+                            </div>
+                            <div className="sign_in">
+                                <h1>Sign In</h1>
+                            </div>
+                        </form>
                         <div className="RD_auth_round">
                             <div>
                                 <h2>R D</h2>
