28edb2bea913efce0a5032888f3c7f6615ba7420
keshav <kdooleea@yahoo.ca>
2020-06-13 12:25:33 -0400

    working on login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import Login from "./components/Login";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact strict path={["/", "/login"]} component={Login}></Route>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../App.css"
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="auth_body">
+                <h1>Sockonnect</h1>
+                <div className="R D">
+                    <h1>R D</h1>
+                </div>
+                <div className="auth_div"></div>
+            </div>
+        );
+    }
+}
+
+export default Login;
