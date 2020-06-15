1550cf91bfe508341041f611f0e727a51854ddaf
keshav <kdooleea@yahoo.ca>
2020-06-14 20:31:48 -0400

    working on home- left side

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import "./Home.css";
 
 class Home extends Component {
 
     render() {
         return (
-            <div>HOME</div>
+            <div className="home_body">
+                <div className="friends_div">
+                    <div className="hamburger"></div>
+                    <h1>FRIENDS</h1>
+                    <div className="friendList"></div>
+                </div>
+                <div className="chat_div"></div>
+                <div className="addFriends_div"></div>
+            </div>
         );
     }
 }
